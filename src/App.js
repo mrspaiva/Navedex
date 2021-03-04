c3f9f6e15766c50091bc9db4fa36c7f4969edeba
Natalia <natyap28@hotmail.com>
2021-03-04 20:07:52 -0300

    add global style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { GlobalStyle } from "./global";
+import Routes from "./routes";
 
 export default function App() {
   return (
-   <h1>Hello World</h1>
+    <div>
+      <GlobalStyle />
+      <Routes />
+    </div>
   );
 }
