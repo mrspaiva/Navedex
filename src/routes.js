36ab7eb80ec5ab13a23667886f80c8e88fbc9841
Natalia <natyap28@hotmail.com>
2021-03-04 19:37:38 -0300

    create routes page

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+      </Switch>
+    </BrowserRouter>
+  )
+}
