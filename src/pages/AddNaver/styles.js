feed35a0f048fb35728426ef8feae64ccf123ee4
Natalia <natyap28@hotmail.com>
2021-03-05 06:37:11 -0300

    add naver style

--- src/pages/AddNaver/styles.js
+++ src/pages/AddNaver/styles.js
@@ -3,6 +3,19 @@ import styled from 'styled-components'
 export const AddNaverContainer = styled.div`
   display: flex;  
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 600;
+`
+
+export const AddTitle = styled.h1`
+  font-size: 1.5rem;
+  margin-top: 2.6rem;
+  margin-left: 1.4rem;
+`
+
+export const MainSection = styled.section`
+  display: flex;
+  align-items: flex-end;
+  margin-left: 21.9rem;
 `
