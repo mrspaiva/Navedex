00d6004cfabb7b782000755b7412e097bfb77c2b
Natalia <natyap28@hotmail.com>
2021-03-05 06:41:03 -0300

    path to logo icon fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { NavbarContainer, Logo } from './NavbarStyle' 
 import { Link } from 'react-router-dom'
-import logo from '../../img/logo.png' 
+import logo from '../img/logo.png' 
 
 export default function Navbar() {
   return (
