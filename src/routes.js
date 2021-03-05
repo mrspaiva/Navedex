e6094ff6c86d411e77341f2556bfc2879e64ef37
Natalia <natyap28@hotmail.com>
2021-03-05 04:56:17 -0300

    add naver page route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import AddNaver from './pages/AddNaver'
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path='/' exact component={Login}/>
+        <Route path='/home' component={Home}/>
+        <Route path='/addnaver' component={AddNaver}/>
       </Switch>
     </BrowserRouter>
   )
