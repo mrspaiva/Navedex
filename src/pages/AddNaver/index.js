97bbf0f63456d4801e020f65eb995ab115a47f6c
Natalia <natyap28@hotmail.com>
2021-03-05 06:44:58 -0300

    add main content

--- src/pages/AddNaver/index.js
+++ src/pages/AddNaver/index.js
@@ -1,7 +1,20 @@
-import {  } from './styles'
+import { Link } from 'react-router-dom'
+import { AddNaverContainer, MainSection, AddTitle } from './styles'
+import Navbar from '../../components/Navbar'
+import arrowLeft from '../../img/arrowLeft.png'
 
 export default function AddNaver() {
   return (
-    <div></div>
+    <AddNaverContainer>
+      <Navbar />
+
+      <MainSection>
+        <Link to='/home'>
+          <img src={arrowLeft} alt=""/>
+        </Link>
+
+        <AddTitle>Adicionar Naver</AddTitle>
+      </MainSection>
+    </AddNaverContainer>
   )
 }
