ea69fe20e6cd2ab0916a13768a816e543a9354fa
Natalia <natyap28@hotmail.com>
2021-03-05 06:35:23 -0300

    add path to homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,26 @@
-import { HomeContainer } from './styles'
+import { Link } from 'react-router-dom'
+import { HomeContainer, ContentSection, Title, AddButton, CardContainer } from './styles'
 import Navbar from '../../components/Navbar'
 import Card from '../../components/Card'
 
 export default function Home() {
   return (
     <HomeContainer>
-      <Navbar />  
-      <Card />
+      <Navbar /> 
+
+      <ContentSection>
+        <Title>Navers</Title> 
+        <Link to='/addnaver'>
+          <AddButton>Adicionar Naver</AddButton>
+        </Link>
+      </ContentSection>
+
+      <CardContainer>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </CardContainer>
     </HomeContainer>
   )
 }
