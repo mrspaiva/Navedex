005c003fab8b1050485e16281201278fee150f93
Natalia <natyap28@hotmail.com>
2021-03-05 06:42:59 -0300

    margin updated

--- src/components/NavbarStyle.js
+++ src/components/NavbarStyle.js
@@ -4,16 +4,16 @@ export const NavbarContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin: 0 2rem;
+  width: 100vw;
 
   font-family: 'Montserrat', sans-serif;
   font-weight: 600;
 
   .exit {
     font-size: 0.9rem;
-    margin-top: 1.8rem;
+    margin: 1.8rem 2rem 1.4rem;
     text-decoration: none;
-    color: #000
+    color: #000;
   }
 
   .exit:hover {
@@ -25,5 +25,6 @@ export const Logo = styled.img`
   width: 9.1rem;
   height: 2.3rem;
   margin-top: 1.4rem;
+  margin: 1.4rem 2rem;
 `
 
