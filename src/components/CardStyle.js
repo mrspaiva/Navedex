db96ee081829b888754d576965c8fe6a7fcb62d8
Natalia <natyap28@hotmail.com>
2021-03-05 01:35:12 -0300

    create card style

---
+++ src/components/CardStyle.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const CardContainer = styled.div`
+  margin-top: 8rem;
+  margin-left: 30rem;
+
+  width: 17.5rem;
+  height: 23.5rem;
+  border: 1px solid #000;
+
+  font-family: 'Montserrat', sans-serif;
+
+  img {
+    height: 17.5rem;
+    margin-bottom: 1rem;
+  }
+
+  strong {
+    font-weight: 600;
+  }
+
+  p {
+    font-weight: 400
+  }
+`
