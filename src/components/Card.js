2507506409ade22b551896aed2202257e5b90740
Natalia <natyap28@hotmail.com>
2021-03-05 01:34:11 -0300

    create card component

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import { CardContainer } from './CardStyle'
+
+export default function Card() {
+  return (
+    <CardContainer>
+      <img src="https://github.com/mrspaiva.png" alt=""/>
+      <strong>Natalia Paiva</strong>
+      <p>Front-end Developer</p>
+    </CardContainer>
+  )
+}
