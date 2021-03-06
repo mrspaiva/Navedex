346416e2269f29a4c6cd9db5db3c981dffd4e6de
Natalia <natyap28@hotmail.com>
2021-03-05 21:19:04 -0300

    add text container

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,21 @@
 import { Link } from 'react-router-dom'
-import { FormContainer } from './FormStyle'
+import { FormContainer, TextContainer, Text } from './FormStyle'
 import arrowLeft from '../img/arrowLeft.png'
 
 
 export default function Form() {
   return (
     <FormContainer>
-      <Link to='/home'>
+
+      <TextContainer>
+        <Link to='/home'>
           <img src={arrowLeft} alt=""/>
         </Link>
+
+        <Text>Adicionar Naver</Text>
+      </TextContainer>
+
+
     </FormContainer>
   )
 }
