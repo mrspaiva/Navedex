a021edc1f4a04b2d8a16e226b541859b97ad854e
Natalia <natyap28@hotmail.com>
2021-03-06 01:00:23 -0300

    pass prop to form component

--- src/pages/AddNaver/index.js
+++ src/pages/AddNaver/index.js
@@ -1,20 +1,13 @@
-import { Link } from 'react-router-dom'
-import { AddNaverContainer, MainSection, AddTitle } from './styles'
+import { AddNaverContainer } from './styles'
 import Navbar from '../../components/Navbar'
-import arrowLeft from '../../img/arrowLeft.png'
+import Form from '../../components/Form'
 
 export default function AddNaver() {
   return (
     <AddNaverContainer>
       <Navbar />
-
-      <MainSection>
-        <Link to='/home'>
-          <img src={arrowLeft} alt=""/>
-        </Link>
-
-        <AddTitle>Adicionar Naver</AddTitle>
-      </MainSection>
+      
+        <Form text="Adicionar"/>
     </AddNaverContainer>
   )
 }
