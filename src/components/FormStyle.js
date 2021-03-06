336b0b87954588779576af83164f5e612988e326
Natalia <natyap28@hotmail.com>
2021-03-05 21:01:58 -0300

    create form style

---
+++ src/components/FormStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const FormContainer = styled.div`
+  width: 37rem;
+  height: 24.4rem;
+
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+
+  border: 1px solid #000
+`
