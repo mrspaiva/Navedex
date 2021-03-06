12689ad8412e6cbfd5e34eb5919b163f24d7ab44
Natalia <natyap28@hotmail.com>
2021-03-05 21:19:45 -0300

    add text style

--- src/components/FormStyle.js
+++ src/components/FormStyle.js
@@ -8,5 +8,19 @@ export const FormContainer = styled.div`
   flex-direction: column;
   align-self: center;
 
-  border: 1px solid #000
+  border: 1px solid #000;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 600;
+`
+
+export const TextContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 2rem;
+
+`
+
+export const Text = styled.h1`
+  font-size: 1.5rem;
+  margin-left: 1.4rem;
 `
