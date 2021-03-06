fcdc8c33f70391432b4531536cd8ab1f24477158
Natalia <natyap28@hotmail.com>
2021-03-06 00:52:42 -0300

    add input container

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom'
-import { FormContainer, TextContainer, Text } from './FormStyle'
+import { FormContainer, TextContainer, Text, InputContainer, SaveButton } from './FormStyle'
 import arrowLeft from '../img/arrowLeft.png'
 
 
-export default function Form() {
+export default function Form(props) {
   return (
     <FormContainer>
 
@@ -12,9 +12,30 @@ export default function Form() {
           <img src={arrowLeft} alt=""/>
         </Link>
 
-        <Text>Adicionar Naver</Text>
+        <Text>{props.text} Naver</Text>
       </TextContainer>
 
+      <InputContainer>
+        <label htmlFor="name">Nome</label>
+        <input type="text" name="name" placeholder="Nome"/>
+        
+        <label htmlFor="idade">Idade</label>
+        <input type="text" name="idade" placeholder="Idade"/>
+        
+        <label htmlFor="p">Projetos que participou</label>
+        <input type="text" name="p" placeholder="Projetos que participou"/>
+        
+        <label htmlFor="cargo">Cargo</label>
+        <input type="text" name="cargo" placeholder="Cargo"/>
+        
+        <label htmlFor="tempo">Tempo de empresa</label>
+        <input type="text" name="cargo" placeholder="Tempo de empresa"/>
+        
+        <label htmlFor="url">URL da foto Naver</label>
+        <input type="text" name="url" placeholder="URL da foto Naver"/>
+      </InputContainer>
+
+      <SaveButton>Salvar</SaveButton>
 
     </FormContainer>
   )
