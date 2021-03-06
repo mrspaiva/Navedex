81fac2e017ba02e211bd0b7cdb56a2017fc3f0dd
Natalia <natyap28@hotmail.com>
2021-03-06 01:55:30 -0300

    add navbar & form component

--- src/pages/EditNaver/index.js
+++ src/pages/EditNaver/index.js
@@ -1,9 +1,13 @@
 import { EditNaverContainer } from './styles'
+import Navbar from '../../components/Navbar'
+import Form from '../../components/Form'
 
 export default function EditNaver() {
   return (
     <EditNaverContainer>
-      
+      <Navbar />
+
+      <Form text="Editar"/>
     </EditNaverContainer>
   )
 }
