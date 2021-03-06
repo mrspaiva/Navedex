ef0eb620dc6ec4c4d4d285511eacf6c2811ff120
Natalia <natyap28@hotmail.com>
2021-03-06 01:20:34 -0300

    add edit page route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import AddNaver from './pages/AddNaver'
+import EditNaver from './pages/EditNaver'
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
         <Route path='/' exact component={Login}/>
         <Route path='/home' component={Home}/>
         <Route path='/addnaver' component={AddNaver}/>
+        <Route path='/editnaver' component={EditNaver}/>
       </Switch>
     </BrowserRouter>
   )
