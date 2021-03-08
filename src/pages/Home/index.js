be2c14fa6936edf82ff113ff0b3a9848bcd6399c
Natalia <natyap28@hotmail.com>
2021-03-08 02:24:45 -0300

    show api data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,29 @@
 import { Link } from 'react-router-dom'
-import { HomeContainer, ContentSection, Title, AddButton, CardContainer } from './styles'
+import { HomeContainer, ContentSection, Title, AddButton, CardWrapper } from './styles'
 import Navbar from '../../components/Navbar'
 import Card from '../../components/Card'
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
 
 export default function Home() {
+  const [navers, setNavers] = useState([])
+
+  const token = localStorage.getItem('@Navedex:Token')
+
+  useEffect(() => {
+    async function fetchData() {
+      api.get('navers', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        setNavers(response.data)
+      })
+    }
+    fetchData()
+  }, [token])
+
   return (
     <HomeContainer>
       <Navbar /> 
@@ -15,12 +35,16 @@ export default function Home() {
         </Link>
       </ContentSection>
 
-      <CardContainer>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </CardContainer>
+      <CardWrapper>
+        {navers.map(naver => (
+            <Card 
+            key={naver.id}
+            img={naver.url}
+            name={naver.name}
+            job={naver.job_role}
+            />
+            ))}
+      </CardWrapper>
     </HomeContainer>
   )
 }
