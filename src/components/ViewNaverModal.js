2a7a8424edf3c86c3a17c744aa7ec4f02e2e16e5
Natalia <natyap28@hotmail.com>
2021-03-10 20:33:07 -0300

    create view modal

--- src/components/ViewNaverModal.js
+++ src/components/ViewNaverModal.js
@@ -1,33 +1,19 @@
-import deleteIcon from '../img/delete.png'
-import updateIcon from '../img/update.png'
+import closeIcon from '../img/close.png'
+import { ViewOverlay, ViewContainer, CloseButton } from './ViewNaverModalStyle'
 
-import { Overlay, ViewContainer, NaverImg, InfoNaver, IconsNaver } from './ViewNaverModalStyle'
+export default function ViewNaverModal ({ isOpen, onClickClose, children }) {
+  if(!isOpen) {
+    return null
+  }
 
-export default function ViewNaverModal() {
   return (
-    <Overlay>
+    <ViewOverlay>
       <ViewContainer>
-        <NaverImg src="https://pyxis.nymag.com/v1/imgs/00a/407/065c31c680d938dd607e42f8f1f8b58c81-15-star-wars-best-moments-2.rsquare.w700.jpg" alt=""/>
-
-        <InfoNaver>
-          <strong>Darth Vader</strong>
-          <span>Front-end Developer</span>
-
-          <p>Idade</p>
-          <span>60</span>
-
-          <p>Tempo de empresa</p>
-          <span>30</span>
-
-          <p>Projetos que participou</p>
-          <span>Guerra Intergalática</span>
-
-          <IconsNaver>
-            <img src={deleteIcon} alt=""/>
-            <img src={updateIcon} alt=""/>
-          </IconsNaver>
-        </InfoNaver>
+        {children}  
+        <CloseButton onClick={onClickClose}>
+          <img  src={closeIcon} alt=""/>
+        </CloseButton>
       </ViewContainer>
-    </Overlay>
+    </ViewOverlay>
   )
 }
