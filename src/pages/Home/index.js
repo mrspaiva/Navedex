b7753dc61813f315abef4cc98fcec31911ca225f
Natalia <natyap28@hotmail.com>
2021-03-11 08:28:35 -0300

    add home page functionally

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,33 @@
-import { Link } from 'react-router-dom'
-import { HomeContainer, ContentSection, Title, AddButton, CardWrapper } from './styles'
+import { Link, useHistory } from 'react-router-dom'
+import moment from 'moment'
 import Navbar from '../../components/Navbar'
 import Card from '../../components/Card'
 import { useEffect, useState } from 'react'
 import api from '../../services/api'
+import ViewNaverModal from '../../components/ViewNaverModal'
+import DeleteNaverModal from '../../components/DeleteNaverModal'
+import FeedbackModal from '../../components/FeedbackModal'
+import deleteIcon from '../../img/delete.png'
+import updateIcon from '../../img/update.png'
+
+import { 
+  HomeContainer, 
+  ContentSection, 
+  Title, 
+  AddButton, 
+  CardWrapper, 
+  NaverImg, 
+  InfoNaver, 
+  IconsNaver
+} from './styles'
 
 export default function Home() {
   const [navers, setNavers] = useState([])
+  const [viewNaver, setViewNaver] = useState(null)
+  const [naverId, setNaverId] = useState(null)
+  const [modalFeedback, setModalFeedback] = useState(false)
 
+  const history = useHistory();
   const token = localStorage.getItem('@Navedex:Token')
 
   useEffect(() => {
@@ -23,6 +43,37 @@ export default function Home() {
     }
     fetchData()
   }, [token])
+
+  async function handleDelete(id) {
+    try {
+      await api.delete(`navers/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      setNavers(navers.filter(naver => naver.id !== id));
+    } catch {
+      alert("Erro ao deletar. Tente novamente.");
+    }
+
+    setNaverId(null)
+    setModalFeedback(true)
+
+    setTimeout(() => {
+      setModalFeedback(false)
+      history.push('/home')
+    }, 1000)
+
+    return <FeedbackModal />
+  }
+
+  function calcAge(date) {
+    const age = Math.floor(
+      moment().diff(moment(date), 'years', true)
+    )
+    return `${age} anos`
+  }
 
   return (
     <HomeContainer>
@@ -40,11 +91,57 @@ export default function Home() {
             <Card 
             key={naver.id}
             img={naver.url}
+            onClickCard={() => setViewNaver(naver)}
             name={naver.name}
             job={naver.job_role}
+            del={() => setNaverId(naver.id)}
+            onClickEdit={() => history.push(`editnaver/${naver.id}`)}
             />
             ))}
       </CardWrapper>
+
+      <ViewNaverModal isOpen={Boolean(viewNaver)} onClickClose={() => setViewNaver(null)}>
+        {viewNaver ? (
+          <>
+            <NaverImg src={viewNaver.url} alt=""/>
+
+            <InfoNaver>
+              <strong>{viewNaver.name}</strong>
+              <span>{viewNaver.job_role}</span>
+
+              <p>Idade</p>
+              <span>{calcAge(viewNaver.birthdate)}</span>
+
+              <p>Tempo de empresa</p>
+              <span>{calcAge(viewNaver.admission_date)}</span>
+
+              <p>Projetos que participou</p>
+              <span>{viewNaver.project}</span>
+
+              <IconsNaver>
+                <img onClick={() => setNaverId(viewNaver.id)} src={deleteIcon} alt=''/>
+                <img 
+                src={updateIcon}
+                onClick={() => history.push(`editnaver/${viewNaver.id}`)}
+                alt='' 
+                />
+              </IconsNaver>
+            </InfoNaver>
+          </>
+        ) : null}  
+      </ViewNaverModal>
+
+      <DeleteNaverModal 
+      isOpen={Boolean(naverId)}
+      onClickClose={() => setNaverId(null)}
+      onClickDelete={() => handleDelete(naverId)}
+      />
+
+      <FeedbackModal 
+      isOpen={modalFeedback} 
+      onClickClose={() => setModalFeedback(false)}
+      text='excluído'
+      />
     </HomeContainer>
   )
 }
