f41535492d8b8169510979717945218b251bc1f0
Natalia <natyap28@hotmail.com>
2021-03-11 08:25:45 -0300

    add page functionally

--- src/pages/AddNaver/index.js
+++ src/pages/AddNaver/index.js
@@ -1,13 +1,144 @@
-import { AddNaverContainer } from './styles'
+import { useHistory } from 'react-router'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import api from '../../services/api'
 import Navbar from '../../components/Navbar'
-import Form from '../../components/Form'
+import FeedbackModal from '../../components/FeedbackModal'
+import arrowLeft from '../../img/arrowLeft.png'
+
+import { 
+  AddNaverContainer, 
+  FormContainer, 
+  TextContainer, 
+  Text, 
+  InputContainer, 
+  SaveButton } from './styles'
 
 export default function AddNaver() {
-  return (
-    <AddNaverContainer>
-      <Navbar />
+  const [name, setName] = useState('')
+  const [birthdate, setBirthdate] = useState('')
+  const [project, setProject] = useState('')
+  const [job_role, setJob_role] = useState('')
+  const [admission_date, setAdmission_date] = useState('')
+  const [url, setUrl] = useState([])
+  const [modalFeedback, setModalFeedback] = useState(false)
+
+  const history = useHistory();
+  const token = localStorage.getItem('@Navedex:Token')
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    const data = {
+      name,
+      birthdate,
+      project,
+      job_role,
+      admission_date,
+      url
+    }
+
+    try {
+      await api.post('navers', data, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      setModalFeedback(true)
+
+      setTimeout(() => {
+        setModalFeedback(false)
+        history.push('/home')
+      }, 1000)
       
-        <Form text="Adicionar"/>
+      } catch (err) {
+        alert("Erro ao cadastrar naver. Tente novamente.")
+      }
+}
+  
+  return (
+      <AddNaverContainer>
+        <Navbar />
+        <FormContainer onSubmit={handleSubmit}>
+
+        <TextContainer>
+          <Link to='/home'>
+            <img src={arrowLeft} alt=""/>
+          </Link>
+
+          <Text>Adicionar Naver</Text>
+        </TextContainer>
+
+        <InputContainer>
+          <label htmlFor="name">Nome</label>
+          <input 
+          required
+          type="text" 
+          name="name" 
+          placeholder="Nome" 
+          value={name} 
+          onChange={e => setName(e.target.value)}
+          />
+          
+          <label htmlFor="idade">Idade</label>
+          <input 
+          required
+          type="date" 
+          name="idade" 
+          placeholder="Idade"
+          value={birthdate} 
+          onChange={e => setBirthdate(e.target.value)}
+          />
+          
+          <label htmlFor="p">Projetos que participou</label>
+          <input 
+          required
+          type="text" 
+          name="p" 
+          placeholder="Projetos que participou"
+          value={project} 
+          onChange={e => setProject(e.target.value)}
+          />
+          
+          <label htmlFor="cargo">Cargo</label>
+          <input 
+          required
+          type="text" 
+          name="cargo" 
+          placeholder="Cargo"
+          value={job_role} 
+          onChange={e => setJob_role(e.target.value)}
+          />
+          
+          <label htmlFor="tempo">Tempo de empresa</label>
+          <input 
+          required
+          type="date" 
+          name="tempo de empresa" 
+          placeholder="Tempo de empresa"
+          value={admission_date} 
+          onChange={e => setAdmission_date(e.target.value)}
+          />
+          
+          <label htmlFor="url">URL da foto Naver</label>
+          <input 
+          required
+          type="text" 
+          name="url" 
+          placeholder="URL da foto Naver"
+          value={url} 
+          onChange={e => setUrl(e.target.value)}
+          />
+        </InputContainer>
+
+        <SaveButton>Salvar</SaveButton>
+
+        <FeedbackModal 
+        isOpen={modalFeedback} 
+        onClickClose={() => setModalFeedback(false)}
+        text='criado'
+        />
+      </FormContainer>
     </AddNaverContainer>
   )
 }
