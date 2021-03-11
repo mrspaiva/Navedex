5d923b01ad8123cfccd1600ffd1cfcdb8d22475f
Natalia <natyap28@hotmail.com>
2021-03-11 08:20:40 -0300

    refactoring card style

--- src/components/CardStyle.js
+++ src/components/CardStyle.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const CardContainer = styled.div`
   width: 17.5rem;
   height: 23.5rem;
+  margin-bottom: 2rem;
 
   font-family: 'Montserrat', sans-serif;
 
@@ -10,6 +11,7 @@ export const CardContainer = styled.div`
     width: 17.5rem;
     height: 17.5rem;
     margin-bottom: 1rem;
+    cursor: pointer;
   }
 
   strong {
@@ -19,12 +21,11 @@ export const CardContainer = styled.div`
   p {
     font-weight: 400
   }
-
 `
-
 export const IconsSection = styled.section`
   .deleteIcon, .updateIcon {
     margin-top: 0.8rem;
+    outline: 0;
   }
 
   .deleteIcon {
@@ -32,6 +33,7 @@ export const IconsSection = styled.section`
     height: 1.1rem;
     margin-left: 0.3rem;
     margin-right: 1rem;
+    cursor: pointer;
   }
 
   .updateIcon {
